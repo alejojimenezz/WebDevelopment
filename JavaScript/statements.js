c20f1de858dad7d4a40d55da76a371e4a716d19c
Alejandro Jiménez <alejandro.jz1312@gmail.com>
2025-08-23 17:32:18 -0500

    Add statements

---
+++ JavaScript/statements.js
@@ -0,0 +1,17 @@
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+
+/*
+var: Variable
+let: Block variable
+const: Block constant
+
+if: Conditional
+switch: Case switching
+for: Loop
+function: Normal function
+return: Exits a function
+try: Error handling
+*/
