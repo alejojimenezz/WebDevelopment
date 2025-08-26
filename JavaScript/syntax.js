b8677c2564d99242703a60548ae7e3df97e9a2f4
alejojimenezz <ajimenezz@unal.edu.co>
2025-08-25 21:01:55 -0500

    Update variables

--- JavaScript/syntax.js
+++ JavaScript/syntax.js
@@ -1,6 +1,10 @@
 // Create variables
 var x;
 let y;
+// var is outdated, use only for older browsers
+// Better use const and let
+// const: not changeable
+// let: changeable
 
 // Use variables
 x = 5;
