8b56c1cfa2af72a8efe8c9dd6ae11c5ae6f4dd31
Alejandro Jiménez <alejandro.jz1312@gmail.com>
2025-09-07 00:28:46 -0500

    Add const

--- JavaScript/syntax.js
+++ JavaScript/syntax.js
@@ -14,3 +14,17 @@ y = 6;
 let z = x + y;
 
 console.log(z);
+
+const PI = 3.141592653589793;
+// PI = 3.14 -> Outputs error
+
+console.log(PI);
+
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
