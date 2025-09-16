2e5303691c67cdecc4e611ad2dcded0be87b2fc6
alejojimenezz <ajimenezz@unal.edu.co>
2025-09-15 21:44:33 -0500

    Add strings

---
+++ JavaScript/strings.js
@@ -0,0 +1,14 @@
+let text = "John Doe";
+
+let length = text.length;
+
+let carName1 = "Volvo XC60";
+let carName2 = 'Volvo XC60';
+
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+let text2 = `He's often called "Johnny"`;
+
+let text3 = "We are the so-called \"Vikings\" from the north.";
