9e7bffaac5da89b4d7ac043da34a0a65fa339358
alejojimenezz <ajimenezz@unal.edu.co>
2025-09-15 21:47:39 -0500

    Add template strings

---
+++ JavaScript/templateStrings.js
@@ -0,0 +1,19 @@
+let text = `Hello World!`;
+
+let text2 = `He's often called "Johnny"`;
+
+let text3 =
+`The quick
+brown fox
+jumps over
+the lazy dog`;
+
+let firstName = "John";
+let lastName = "Doe";
+
+let text4 = `Welcome ${firstName}, ${lastName}!`;
+
+let price = 10;
+let VAT = 0.25;
+
+let total = `Total: ${(price * (1 + VAT)).toFixed(2)}`;
